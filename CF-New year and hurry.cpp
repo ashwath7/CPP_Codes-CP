6e0de50186c1150ad89f660ca50b024537b6089c
ashwath7 <83606243+ashwath7@users.noreply.github.com>
2021-09-24 21:42:08 +0530

    Add files via upload

---
+++ CF-New year and hurry.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#define int         int64_t
+#define pb          push_back
+#define endl        '\n'
+#define pll         pair<int,int>
+#define vll          vector<int>
+#define all(a)      (a).begin(),(a).end()
+#define x           first
+#define y           second
+#define sz(x)       (int)x.size()
+#define hell        1000000007
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define bs          binary_search
+#define mi        map<int,int>
+#define gcd(a,b)    __gcd((a),(b))
+#define lcm(a,b)    ((a)*(b)) / gcd((a),(b))
+#define rep(i,a,b)  for(int i=a;i<b;i++)
+#define ios     ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+bool check(int mid,int k) {
+	return k + 5*(mid*(mid+1)/2) <=240;
+}
+void solve() {
+	int n , k ; cin>>n>>k;
+    int l=0 , h =n , mid , ans =0;
+    while(h >=l) {
+        mid = (l + h)/2;
+        if(check(mid , k)){
+        	ans = mid;
+        	l = mid + 1;
+        }
+        else {
+        	h = mid -1;
+        }
+    }
+    cout<<ans<<endl;
+}
+signed main(){
+    ios
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("display.txt", "w", stdout);
+    #endif 
+        
+    int test=1;
+    //cin>>test;
+    while(test--){
+        solve();
+    }    
+    return 0;
+}
