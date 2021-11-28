43e9daba3d8b14028eb1b7989a54feea0a8fd435
ashwath7 <83606243+ashwath7@users.noreply.github.com>
2021-11-28 23:34:03 +0530

    Add files via upload

---
+++ bs_floor_of_a_number.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+#define int         int64_t
+#define pb          push_back
+#define endl        '\n'
+#define pll         pair<int,int>
+#define vll          vector<int>
+#define all(a)      (a).begin(),(a).end()
+#define x           first
+#define y           second
+#define sz(x)       (int)x.size()
+#define hell        1000000007
+#define lbnd        lower_bound
+#define ubnd        upper_bound
+#define bs          binary_search
+#define mi        map<int,int>
+#define gcd(a,b)    __gcd((a),(b))
+#define lcm(a,b)    ((a)*(b)) / gcd((a),(b))
+#define rep(i,a,b)  for(int i=a;i<b;i++)
+#define ios     ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+bool check(vector<int> a,int mid, int x){
+	return a[mid] <=x;
+}
+void solve(){
+    int n,x; cin>>n>>x;
+    vector<int> a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    int l = 0, h = n-1 , mid ,ans;
+    while(h>=l){
+    	mid = l + (h-l)/2;
+    	if(check(a,mid,x)){
+    		ans = mid;
+    		l = mid+1;
+
+    	}
+    	else h = mid-1;
+    }	
+    cout<<a[ans]<<endl;
+    
+}
+signed main(){
+    
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("display.txt", "w", stdout);
+    #endif 
+        
+    int test=1;
+    //cin>>test;
+    while(test--){
+        solve();
+    }    
+    return 0;
+}
